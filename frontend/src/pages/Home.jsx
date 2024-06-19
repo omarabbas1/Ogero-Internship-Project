b065d6960e4f7c3a27b701eed9a30054dc8c8795
omarabbas1 <omarfadiabbas@outlook.com>
2024-06-19 19:55:13 +0300

    minor style changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1 className="home-title">Welcome to Our Streaming Service!</h1>
-      <p className="home-text">
-        Watch your favorite movies and TV shows anytime, anywhere.
-      </p>
-      <p className="home-text">Sign up now to start streaming!</p>
+    <div className="home-wrapper">
+      <div className="home-container">
+        <h1 className="home-title">Welcome to Our Streaming Service!</h1>
+        <p className="home-text">
+          Watch your favorite movies and TV shows anytime, anywhere.
+        </p>
+        <p className="home-text">Sign up now to start streaming!</p>
+      </div>
     </div>
   );
 };
